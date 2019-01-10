568acae71f37940efea143b6d3d2448a7572ac0e
Bonkles <bennyc@gmail.com>
2019-01-10 17:56:53 -0500

    Basic skeletal structure for our data transfer objects (DTOs), and an empty implementation of our GET method.

--- gohax.go
+++ gohax.go
@@ -5,5 +5,28 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Let's do some invitation figuring-outting!")
+
+	GetPartners()
+}
+
+var getUrl = "http://https://candidate.hubteam.com/candidateTest/v3/problem/dataset?userKey=ccbae50eb46fe5cc58ff2d925903"
+
+//This struct will represent every element in the GET response list.
+type Partner struct {
+	FirstName      string   `json:"firstName"`
+	LastName       string   `json:"lastName"`
+	Email          string   `json:"email"`
+	Country        string   `json:"country"`
+	AvailableDates []string `json:"availableDates"`
+}
+
+//This struct is for the entire list response.
+type Partners struct {
+	Partners []Partner `json:"partners"`
+}
+
+func GetPartners() {
+	fmt.Println("About to issue GET request to the hubteam server at URL: ", getUrl)
+
 }
